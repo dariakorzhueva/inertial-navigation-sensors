7fc68c522114b5d37b5ee6ffe521f0a94316bd14
DariaKorzhueva <korzhueva.daria@gmail.com>
2020-05-08 01:13:38 +0700

    Refactor variables

--- app/src/main/java/com/korzhueva/android/inertialnavigation/filters/AlphaBetaFilter.java
+++ app/src/main/java/com/korzhueva/android/inertialnavigation/filters/AlphaBetaFilter.java
@@ -2,40 +2,36 @@ package com.korzhueva.android.inertialnavigation.filters;
 
 public class AlphaBetaFilter {
     private double dt = 0.5;
-    private double xk1 = 0, vk1 = 0, a = 0.85, b = 0.005;
-    private double xk, vk, rk;
+    private double ak1 = 0, jk1 = 0, a = 0.85, b = 0.005;
+    private double ak, jk, rk;
 
-    public AlphaBetaFilter() {
-
-    }
-
-    public AlphaBetaFilter(double dt, double xk1, double vk1, double a, double b) {
+    public AlphaBetaFilter(double dt, double ak1, double jk1, double a, double b) {
         this.dt = dt;
-        this.xk1 = xk1;
-        this.vk1 = vk1;
+        this.ak1 = ak1;
+        this.jk1 = jk1;
         this.a = a;
         this.b = b;
     }
 
     public double update(double current) {
-        xk = xk1 + (vk1 * dt);
-        vk = vk1;
+        ak = ak1 + (jk1 * dt);
+        jk = jk1;
 
-        rk = current - xk;
+        rk = current - ak;
 
-        xk += a * rk;
-        vk += (b * rk) / dt;
+        ak += a * rk;
+        jk += (b * rk) / dt;
 
-        xk1 = xk;
-        vk1 = vk;
+        ak1 = ak;
+        jk1 = jk;
 
-        return xk1;
+        return ak1;
     }
 
     public void reset() {
         dt = 0;
-        xk1 = 0;
-        vk1 = 0;
+        ak1 = 0;
+        jk1 = 0;
         a = 0;
         b = 0;
     }
